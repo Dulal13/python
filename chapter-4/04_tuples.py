85bf8f22bbbb9d081784fff3db7fdfcdf7d87957
Dulal <dulalmiah70112@gmail.com>
2021-08-10 09:09:38 +0600

    done chapter 4 , here i am learn about list and tuples

---
+++ chapter-4/04_tuples.py
@@ -0,0 +1,12 @@
+# Creating a tuple using ()
+t = (1,3,4,5)
+
+# Printing the elements of a tuple
+print(t[0])
+
+# Cannot update the tuple value
+# t[0] = 23 throws an error
+tuples = (1,) # Tuple with single element
+# tuples = (1) wrong way the declare tuple with single element
+
+print(tuples)
---
+++ chapter-4/05_tuples_method.py
@@ -0,0 +1,6 @@
+# Creating a tuple using ()
+t = (1,3,4,5,1,1,2,2)
+
+print(t.count(1))
+print(t.count(2))
+print(t.index(4))
